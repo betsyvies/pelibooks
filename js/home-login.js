b685b6f7e39487f2a3f04daba56aaa510e1481bd
Gloper98 <littleannygutlop_2014@outlook.es>
2018-01-18 14:36:50 -0500

    Updating files

---
+++ js/home-login.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+  // modal login
+  var $email = $('#email-login');
+  var $password = $('#psw-login');
+  var $buttonSubmit = $('#login-button');
+
+  // habilitar el boton  
+  var validateEmailLogin = false;
+  var validatePasswordLogin = false;
+
+  // llamamos a los valores guardados en el localStorage
+  console.log(localStorage.name);
+  console.log(localStorage.email);
+  console.log(localStorage.password);
+
+  $email.on('input', function () {
+    if ($(this).val() === localStorage.email) {
+      alert('ok email');
+      validateEmailLogin = true;
+    }
+  });
+
+  $password.on('input', function () {
+    if ($(this).val() === localStorage.password) {
+      alert('ok password');
+      validatePasswordLogin = true;
+    }
+  });
+
+  $buttonSubmit.on('click', function (event) {
+    event.preventDefault();
+    if (validateEmailLogin && validatePasswordLogin) {
+      alert('Ya eres parte de Pelibooks!!');
+      window.location.href = 'vista-home.html';
+    } else {
+      alert('Regístrate por favor');
+    }
+
+  });
+});
+
+
+
+
