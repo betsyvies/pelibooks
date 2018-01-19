c0823d4dedf5b46bfe381d05b1c9b32fd5935777
betsyvies <ing.betsyvies@gmail.com>
2018-01-19 11:24:29 -0500

    añadiendo novels a la data

--- js/vista-movie.js
+++ js/vista-movie.js
@@ -1,6 +1,16 @@
+$(document).ready(function() { 
+  getMovie(); 
+
+  $inputSearch = ('#input-search');
+
+  $inputSearch.on('keyup', function() {
+    let searchText = $(this).val();
+  });
+});
+
 // $(function() {
 //   $('#rateYo').rateYo({
 //     rating: 3.6
 //   });
 // });
-getMovie();  
+
